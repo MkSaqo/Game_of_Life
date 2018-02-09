3845d502c94aff4e1346edfcab913306d33d1369
MkSaqo <sargis.mkrtchyan1.y@tumo.org>
2018-02-09 22:03:54 +0400

    first commit

---
+++ class.js
@@ -0,0 +1,340 @@
+class Grass {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+    }
+    stanalNorKordinatner() {
+        this.diellipseions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    yntrelVandak(ch) {
+
+        var found = [];
+        for (var i in this.diellipseions) {
+            var x = this.diellipseions[i][0];
+            var y = this.diellipseions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == ch) {
+                    found.push(this.diellipseions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    bazmanal() {
+        this.stanalNorKordinatner();
+        var norVandak = random(this.yntrelVandak(0));
+        this.multiply++;
+        if (this.multiply > 8 && norVandak) {
+            var norXot = new Grass(norVandak[0], norVandak[1]);
+            grassArr.push(norXot);
+            matrix[norVandak[1]][norVandak[0]] = 1;
+            this.multiply = 0;
+        }
+    }
+
+}
+////////////////////////////////////////////////////////////////////////
+////////////////////XOTAKER//////////XOTAKER////////////////////////////
+////////////////////////////////////////////////////////////////////////
+class Xotaker {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 5;
+    }
+    stanalNorKordinatner() {
+        this.diellipseions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    yntrelVandak(ch) {
+        var found = [];
+        for (var i in this.diellipseions) {
+            var x = this.diellipseions[i][0];
+            var y = this.diellipseions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == ch) {
+                    found.push(this.diellipseions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    utel() {
+        this.stanalNorKordinatner();
+        var norVandak = random(this.yntrelVandak(1));
+        if (norVandak) {
+            for (var i in grassArr) {
+                if (norVandak[0] == grassArr[i].x && norVandak[1] == grassArr[i].y) {
+                    grassArr.splice(i, 1);
+                    break;
+                }
+            }
+            matrix[this.y][this.x] = 0;
+            this.x = norVandak[0];
+            this.y = norVandak[1];
+            matrix[this.y][this.x] = 2;
+            this.energy++;
+            this.bazmanal();
+        }
+        else {
+            this.sharjvel();
+        }
+    }
+    sharjvel() {
+        this.stanalNorKordinatner();
+        var norVandak = random(this.yntrelVandak(0));
+        if (norVandak) {
+            matrix[this.y][this.x] = 0;
+            this.x = norVandak[0];
+            this.y = norVandak[1];
+            matrix[this.y][this.x] = 2;
+            this.energy--;
+            this.satkel();
+        }
+
+    }
+    bazmanal() {
+        var norVandak = random(this.yntrelVandak(0));
+        if (norVandak && this.energy > 10) {
+            matrix[norVandak[1]][norVandak[0]] = 2;
+            xkArr.push(new Xotaker(norVandak[0], norVandak[1]));
+            this.energy = 5;
+        }
+    }
+    satkel() {
+        if (this.energy < 0) {
+            for (var i in xkArr) {
+                if (this.x == xkArr[i].x && this.y == xkArr[i].y) {
+                    xkArr.splice(i, 1);
+                }
+            }
+            matrix[this.y][this.x] = 0;
+        }
+    }
+
+
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////
+////////////////////PAT//////////PAT//////////////PAT////////PAT////////
+////////////////////////////////////////////////////////////////////////
+
+
+
+class Pat {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+    }
+
+    pat_sharjvel() {
+        var pat = this.randVandak();
+        this.multiply++;
+        if (this.multiply >= 6) {
+            if (pat) {
+                this.x = pat[0];
+                this.y = pat[1];
+                matrix[this.y][this.x] = 3;
+                this.multiply = 0;
+                for (var c in grassArr) {
+                    if ((grassArr[c].x == this.x) && (grassArr[c].y == this.y)) {
+                        grassArr.splice(c, 1);
+                    }
+                }
+
+                for (var c in xkArr) {
+                    if ((xkArr[c].x == this.x) && (xkArr[c].y == this.y)) {
+                        xkArr.splice(c, 1);
+                    }
+                }
+            }
+
+            else {
+                patArr.pop();
+            }
+
+            // }
+        }
+    }
+    randVandak() {
+        var x = Math.floor(Math.random() * matrix.length);
+        var y = Math.floor(Math.random() * matrix[0].length);
+
+        if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+            var arr = [x, y];
+            return arr;
+        }
+
+    }
+}
+////////////////////////////////////////////////////////////////////////
+////////////////////Xotakeraker//////////Xotakeraker////////////////////
+////////////////////////////////////////////////////////////////////////
+class Xotakeraker {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    stanalNorKordinatner() {
+        this.diellipseions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    yntrelVandak(ch) {
+        var found = [];
+        for (var i in this.diellipseions) {
+            var x = this.diellipseions[i][0];
+            var y = this.diellipseions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == ch) {
+                    found.push(this.diellipseions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    utel() {
+        this.stanalNorKordinatner();
+        var norVandak = random(this.yntrelVandak(2));
+        if (norVandak) {
+            matrix[this.y][this.x] = 0;
+            this.x = norVandak[0];
+            this.y = norVandak[1];
+            matrix[this.y][this.x] = 4;
+            for (var i in xkArr) {
+                if (this.x == xkArr[i].x && this.y == xkArr[i].y) {
+                    xkArr.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+    }
+}
+////////////////////////////////////////////////////////////////////////
+////////////////////Carukyan//////////Carukyan////////////////////
+////////////////////////////////////////////////////////////////////////
+class Carukyan {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    stanalNorKordinatner() {
+        this.diellipseions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    yntrelVandak(ch) {
+        var found = [];
+        for (var i in this.diellipseions) {
+            var x = this.diellipseions[i][0];
+            var y = this.diellipseions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == ch) {
+                    found.push(this.diellipseions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    yntrelVandak1() {
+        var found = [];
+        for (var i in this.diellipseions) {
+            var x = this.diellipseions[i][0];
+            var y = this.diellipseions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                found.push(this.diellipseions[i]);
+            }
+        }
+        return found;
+    }
+    baregorcutun() {
+        this.stanalNorKordinatner();
+        var norVandak = random(this.yntrelVandak(3));
+        if (norVandak) {
+            matrix[this.y][this.x] = 2;
+            xkArr.push(new Xotaker(this.x, this.y));
+            this.x = norVandak[0];
+            this.y = norVandak[1];
+            matrix[this.y][this.x] = 5;
+        }
+        else {
+            this.sharjvel();
+        }
+    }
+    sharjvel() {
+        this.stanalNorKordinatner();
+        var norVandak = random(this.yntrelVandak1());
+        if (norVandak) {
+            matrix[this.y][this.x] = 0;
+            this.x = norVandak[0];
+            this.y = norVandak[1];
+
+            console.log(matrix[this.y][this.x])
+            if(matrix[this.y][this.x]==1){
+                for (var i in grassArr) {
+                    if (this.x == grassArr[i].x && this.y == grassArr[i].y) {
+                        grassArr.splice(i, 1);
+                        // console.log(grassArr)
+                        break;
+                    }
+                }
+            }
+            else if(matrix[this.y][this.x]==2){
+                for (var i in xkArr) {
+                    if (this.x == xkArr[i].x && this.y == xkArr[i].y) {
+                        xkArr.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+            else if(matrix[this.y][this.x]==4){
+                for (var i in xotakerakerArr) {
+                    if (this.x == xotakerakerArr[i].x && this.y == xotakerakerArr[i].y) {
+                        xotakerakerArr.splice(i, 1);
+                        console.log(xotakerakerArr)
+                        break;
+                    }
+                }
+            }
+            matrix[this.y][this.x] = 5;
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,107 @@
+var matrix = matrica(15, 15);
+var side = 70;
+var grassArr = [];
+var xkArr = [];
+var patArr = [];
+var xotakerakerArr = [];
+var carukyanArr = [];
+function setup() {
+    createCanvas(matrix.length * side, matrix[0].length * side);
+    background("grey");
+    frameRate(7);
+    noStroke()
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                grassArr.push(new Grass(x,y));
+            }
+            else if (matrix[y][x] == 2) {
+                xkArr.push(new Xotaker(x,y));
+            }
+            else if (matrix[y][x] == 3) {
+                patArr.push(new Pat(x,y));
+            }
+            
+            else if (matrix[y][x] == 4) {
+                xotakerakerArr.push(new Xotakeraker(x,y));
+            }
+            
+            else if (matrix[y][x] == 5) {
+                carukyanArr.push(new Carukyan(x,y));
+            }
+        }
+    }
+}
+function draw() {
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix.length; ++x) {
+            if (matrix[y][x] == 1) {
+                fill("green");
+                ellipse(x * side +side/2, y * side+side/2, side, side);
+            }
+            else if (matrix[y][x] == 0) {
+                fill("gray");
+                ellipse(x * side +side/2, y * side+side/2, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                ellipse(x * side+side/2, y * side+side/2, side, side);
+            }
+            
+            else if (matrix[y][x] == 3) {
+                fill("black");
+                ellipse(x * side+side/2, y * side+side/2, side, side);
+            }
+            
+            else if (matrix[y][x] == 4) {
+                fill("red");
+                ellipse(x * side+side/2, y * side+side/2, side, side);
+            }
+            
+            else if (matrix[y][x] == 5) {
+                fill("lightblue");
+                ellipse(x * side+side/2, y * side +side/2, side, side);
+            }
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].bazmanal();
+    }
+
+    for (var i in xkArr) {
+       xkArr[i].utel();
+    }
+    for (var i in xotakerakerArr) {
+        xotakerakerArr[i].utel();
+    }
+
+    for (var i in patArr) {
+        patArr[i].pat_sharjvel();
+    }
+
+    for (var i in carukyanArr ) {
+        carukyanArr[i].baregorcutun();
+    }
+}
+
+function matrica(n, m) {
+    var matrix = [];
+    for (var i = 0; i < n; i++) {
+        matrix.push([]);
+        for (var j = 0; j < m; j++) {
+            matrix[i][j] = Math.floor(Math.random() * 2);
+        }
+    }
+    
+    matrix[Math.floor(Math.random()*matrix.length)][Math.floor(Math.random()*matrix[0].length)] = 2;
+    matrix[Math.floor(Math.random()*matrix.length)][Math.floor(Math.random()*matrix[0].length)] = 2;
+    matrix[Math.floor(Math.random()*matrix.length)][Math.floor(Math.random()*matrix[0].length)] = 2;
+    matrix[Math.floor(Math.random()*matrix.length)][Math.floor(Math.random()*matrix[0].length)] = 2;
+    matrix[Math.floor(Math.random()*matrix.length)][Math.floor(Math.random()*matrix[0].length)] = 3;
+    matrix[Math.floor(Math.random()*matrix.length)][Math.floor(Math.random()*matrix[0].length)] = 5;
+    matrix[Math.floor(matrix.length/4)][Math.floor(Math.random()*matrix[0].length)] = 4;
+    matrix[Math.floor(matrix.length-3)][Math.floor(Math.random()*matrix[0].length)] = 4;
+    matrix[Math.floor(matrix.length-7)][Math.floor(Math.random()*matrix[0].length)] = 4;
+    return matrix;
+}
+
